34fe320fc9fea04dee407d3110b6f2d840fc3d86
vishal thakur <86705054+htr3@users.noreply.github.com>
2022-08-07 18:40:13 +0530

    Create StandardProblems.cpp

---
+++ StandardProblems.cpp
@@ -0,0 +1,39 @@
+// all standard problems for placement 
+
+// Linked List 
+
+
+
+
+// Array 
+
+
+
+// Dynamic Programming 
+
+
+
+
+// Recursion & Backtracking
+
+
+
+
+// Stack And Queue 
+
+
+
+// String 
+
+
+
+
+// Graph 
+
+
+
+//Trie
+
+
+
+//
