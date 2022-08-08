0c1ec3ceeb9f1fa56d9434c172fd91882ab4c2b4
vishal thakur <86705054+htr3@users.noreply.github.com>
2022-08-08 10:39:07 +0530

    Update StandardProblems.cpp

--- StandardProblems.cpp
+++ StandardProblems.cpp
@@ -6,6 +6,40 @@
 
 
 // Array 
+ 
+// Problem 1. Spiral matrix 
+  
+void print(int arr[R][C], int i, int j, int m, int n)
+{
+    // If i or j lies outside the matrix
+    if (i >= m or j >= n)
+        return;
+  
+    // Print First Row
+    for (int p = j; p < n; p++)
+        cout << arr[i][p] << " ";
+  
+    // Print Last Column
+    for (int p = i + 1; p < m; p++)
+        cout << arr[p][n - 1] << " ";
+  
+    // Print Last Row, if Last and
+    // First Row are not same
+    if ((m - 1) != i)
+        for (int p = n - 2; p >= j; p--)
+            cout << arr[m - 1][p] << " ";
+  
+    // Print First Column,  if Last and
+    // First Column are not same
+    if ((n - 1) != j)
+        for (int p = m - 2; p > i; p--)
+            cout << arr[p][j] << " ";
+  
+    print(arr, i + 1, j + 1, m - 1, n - 1);
+}
+// ling-> https://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/
+
+// Problem 2 
 
 
 
