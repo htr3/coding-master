01125f3cab77ff0e5ec221fea104e462ab4c5bd0
vishal thakur <86705054+htr3@users.noreply.github.com>
2022-08-10 18:51:53 +0530

    Update StandardProblems.cpp

--- StandardProblems.cpp
+++ StandardProblems.cpp
@@ -349,6 +349,30 @@ int main()
 //  =================================================================================================
 //  ==================================================================================================
 
+// Problem 1. House rober -> 
+
+int Util(vector<int>& nums, int i , vector<int>& dp)  {
+        
+        if(i>=nums.size()) return 0; 
+        
+        if(dp[i]!=-1) return dp[i]; 
+        
+        int including = nums[i] + Util(nums,i+2,dp); 
+        int excluding = Util(nums,i+1,dp); 
+        
+        return dp[i] =max(including,excluding); 
+    }
+    
+    
+    int rob(vector<int>& nums) {
+        
+        int n = nums.size(); 
+        vector<int> dp(n,-1);
+        
+           return Util(nums,0,dp); 
+        
+    }
+
 
 
 
