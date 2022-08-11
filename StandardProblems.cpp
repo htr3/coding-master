e8262f75742d33b2675b8b7201b66cd2e999a1ec
vishal thakur <86705054+htr3@users.noreply.github.com>
2022-08-11 11:52:59 +0530

    Update StandardProblems.cpp

--- StandardProblems.cpp
+++ StandardProblems.cpp
@@ -411,6 +411,24 @@ int Util(vector<int>& nums, int i , vector<int>& dp)  {
 //  =================================================================================================
 //  ==================================================================================================
 
+// Problem 1. Print all substring of string  
+
+void printSubstrings(string str)
+{
+ 
+    // First loop for starting index
+    for (int i = 0; i < str.length(); i++) {
+        string subStr;
+        // Second loop is generating sub-string
+        for (int j = i; j < str.length(); j++) {
+            subStr += str[j];
+            cout << subStr << endl;
+        }
+    }
+}
+complexity O(n*n) ; 
+//
+
 
 
 
